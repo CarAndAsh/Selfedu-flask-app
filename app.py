87641313e80b0e1178e6fa3b3991a011b3bf4efc
CarAndAsh <zeixes@gmail.com>
2023-06-05 13:31:48 +0300

    Form with HTML. Variants of categories of flash messages.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'q12we34rt56y'
 b_app = Bootstrap(app)
-
 
 links = {'/': 'Главная',
          '/materials': 'Статьи',
@@ -13,7 +13,31 @@ links = {'/': 'Главная',
 
 @app.route('/')
 def main_page():
-    return render_template('base.html', links=links)
+    return render_template('main.html', links=links)
+
+
+@app.route('/materials')
+def materials_page():
+    return render_template('materials.html', links=links)
+
+
+@app.route('/about')
+def info_page():
+    return render_template('about.html', links=links)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login_page():
+    if request.method == 'POST':
+        username = request.form['username']
+        cat = 'warning'
+        if username:
+            cat = 'success'
+            msg = f'Вы попытались зарегистрироваться или войти как {username}'
+        else:
+            msg = f'Пожалуйста, введите логин и пароль'
+        flash(msg, cat)
+    return render_template('login.html', links=links)
 
 
 @app.errorhandler(404)
